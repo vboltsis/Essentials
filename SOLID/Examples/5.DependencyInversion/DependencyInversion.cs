d7016b47e7ae48e5b5de206abef2aed439bde540
Evangelos Boltsis <vbk1313@hotmail.com>
2022-06-23 17:13:29 +0300

    -Added DIP example

--- SOLID/Examples/5.DependencyInversion/DependencyInversion.cs
+++ SOLID/Examples/5.DependencyInversion/DependencyInversion.cs
@@ -1,4 +1,38 @@
-﻿public record DependencyInversion
+﻿/*
+The Dependency Inversion Principle (DIP) states that high level modules,
+should not depend on low level modules; both should depend on abstractions.
+Abstractions should not depend on details.
+Details should depend upon abstractions.
+ */
+public record DependencyInversion
 {
+    public static void Example()
+    {
+        var post = new Post(new LogToDB());
+
+        var secondPost = new Post(new LogToFile());
+    }
 }
 
+public record Post
+{
+    public Post(Ilogger logger)
+    {
+        
+    }
+}
+
+public interface Ilogger
+{
+    void LogError();
+}
+
+public class LogToDB : Ilogger
+{
+    public void LogError() { }// --> logs to db
+}
+
+class LogToFile : Ilogger
+{
+    public void LogError() { } //--> logs to File
+}
