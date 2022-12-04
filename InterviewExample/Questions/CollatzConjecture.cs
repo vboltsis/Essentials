de7a60963b24c5ed0dd8b3f8e87c9ce37053e577
Evangelos Boltsis <vbk1313@hotmail.com>
2022-12-04 17:27:07 +0200

    -added better comment

--- InterviewExample/Questions/CollatzConjecture.cs
+++ InterviewExample/Questions/CollatzConjecture.cs
@@ -1,11 +1,12 @@
 ﻿namespace InterviewExample.Questions;
 
+//Find how many steps we need to get to 1
+//ODD --> 1,3,5,7,9
+//EVEN --> 2,4,6,8,10
+// IF NUMBER IS ODD -> * 3 + 1
+// IF NUMBER EVEN -> / 2
 public class CollatzConjecture
 {
-    //ODD --> 1,3,5,7,9
-    //EVEN --> 2,4,6,8,10
-    // IF NUMBER IS ODD -> * 3 + 1
-    // IF NUMBER EVEN -> / 2
     public static int GetSteps(long number)
     {
         var steps = 0;
