51bf5cb3cdce641c211a672a954473d9698c92a2
Evangelos Boltsis <vbk1313@hotmail.com>
2023-01-28 15:45:17 +0200

    -Added xunit examples

---
+++ XUnitTests/Calculator.cs
@@ -0,0 +1,19 @@
+﻿namespace XUnitTests;
+
+internal class Calculator
+{
+    internal int Addition(params int[] numbers)
+    {
+        return numbers.Sum();
+    }
+
+    internal int Subtraction(int firstNumber, int secondNumber)
+    {
+        return firstNumber - secondNumber;
+    }
+
+    internal int Division(int firstNumber, int secondNumber)
+    {
+        return firstNumber / secondNumber;
+    }
+}
---
+++ XUnitTests/CalculatorTests.cs
@@ -0,0 +1,42 @@
+namespace XUnitTests;
+
+public class CalculatorTests
+{
+    private Calculator _calculator = new Calculator();
+
+    [Theory]
+    [InlineData(10, 2, 8)]
+    [InlineData(3, 2, 1)]
+    public void Subtract_TwoNumbers_Success(int firstNumber, int secondNumber, int expected)
+    {
+        //Act
+        var result = _calculator.Subtraction(firstNumber, secondNumber);
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void Subtract_TwoNumbers_Failure()
+    {
+        //Arrange
+        var firstNumber = 15;
+        var secondNumber = 15;
+        var expected = 4;
+
+        //Act
+        var result = _calculator.Subtraction(firstNumber, secondNumber);
+
+        //Assert
+        Assert.NotEqual(expected, result);
+    }
+
+    [Fact]
+    public void Division_WithZero_ShouldThrowException()
+    {
+        var firstNumber = 1;
+        var secondNumber = 0;
+
+        Assert.Throws<DivideByZeroException>(() => _calculator.Division(firstNumber, secondNumber));
+    }
+}
