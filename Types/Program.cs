d5d5ef6297d3fb9756059ecc9d5d9a46bbd7585f
Evangelos Boltsis <v.boltsis@kaizengaming.com>
2023-07-03 10:58:16 +0300

    -cleanup program.cs

--- Types/Program.cs
+++ Types/Program.cs
@@ -1,60 +1,4 @@
-﻿/*
-bool: a Boolean value that can be either true or false.
-byte: an 8-bit unsigned integer.
-char: a 16-bit Unicode character.
-decimal: a 128-bit precise decimal value.
-double: a 64-bit double-precision floating-point number.
-float: a 32-bit single-precision floating-point number.
-int: a 32-bit signed integer. 
-long: a 64-bit signed integer.
-
-The main difference between signed and unsigned integers is the range of values that they can represent.
-
-A signed integer is a whole number that can be positive, negative, or zero,
-and it uses the leftmost bit to represent the sign (0 for positive and 1 for negative).
-For example, an int is a signed integer that can range from -2147483648 to 2147483647.
-
-On the other hand, an unsigned integer is a whole number that can only be positive or zero,
-and it does not use the leftmost bit to represent the sign.
-Instead, all of the bits are used to represent the magnitude of the number.
-For example, a uint (unsigned integer) can range from 0 to 4294967295.
-
-Signed integers are useful when you need to represent both positive and negative values,
-while unsigned integers are useful when you only need to represent positive values or zero.
-
-C# also has unsigned versions of the other integer types: byte, short, and long.
-These types are similar to their signed counterparts,
-but they use all of their bits to represent the magnitude of the number and
-do not use the leftmost bit to represent the sign.
- */
-
-//bool flag = true;
-//bool result = false;
-
-//byte age = 35;
-//byte maxSpeed = 255;
-
-//int count = 10;
-//uint uCount = 15;
-//int number = -5;
-
-//char letter = 'A';
-//char symbol = '#';
-//int character = 'W';
-
-//double pi = 3.14159;
-//double average = 86.5;
-
-//decimal price = 19.99m;
-//decimal balance = 100.00m;
-
-//float temperature = 72.5f;
-//float distance = 3.9f;
-
-//long population = 8000000000;
-//long scandals = 999999999;
-
-using Types;
+﻿using Types;
 
 int number = 1;
 int? nullable = null;
@@ -65,14 +9,7 @@ Person person = new Person { Name = "1", Age = 1 };
 byte[] bytes = new byte[4];
 PersonStruct personStruct = new PersonStruct { Name = "1", Age = 1 };
 
-
 ByValueOrByReference.ByValue(number, nullable, date, text, list, person, bytes, personStruct);
 ByValueOrByReference.ByReference(ref number, ref nullable, ref date, ref text, ref list, ref person, ref bytes, ref personStruct);
 
 Console.WriteLine();
-
-
-//sortedDictionary - sortedList
-//sortedSet
-//stack - queue
-
