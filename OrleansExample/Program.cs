e0a89162ad3484bf66756ce1f06a0e5ce7d21891
Evangelos Boltsis <v.boltsis@kaizengaming.com>
2023-07-10 12:32:19 +0300

    enumerables

--- OrleansExample/Program.cs
+++ OrleansExample/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Orleans.Concurrency;
+using System.Diagnostics;
 
 // Configure the host
 using var host = new HostBuilder()
@@ -13,15 +15,16 @@ await host.StartAsync();
 var grainFactory = host.Services.GetRequiredService<IGrainFactory>();
 
 // Get a reference to the HelloGrain grain with the key "friend"
-var friend = grainFactory.GetGrain<IHelloGrain>("friend");
+var friendGrain = grainFactory.GetGrain<IHelloGrain>("friend");
 
 // Call the grain and print the result to the console
-var result = await friend.SayHello("Good morning!");
-Console.WriteLine($"""
+var result = friendGrain.SayHello("Good morning!");
+var d = result.IsCompletedSuccessfully;
 
-    {result}
+var greeting = await friendGrain.GetGreeting();
 
-    """);
+
+
 
 Console.WriteLine("Orleans is running.\nPress Enter to terminate...");
 Console.ReadLine();
@@ -31,11 +34,37 @@ await host.StopAsync();
 
 public sealed class HelloGrain : Grain, IHelloGrain
 {
-    public ValueTask<string> SayHello(string greeting) =>
-        ValueTask.FromResult($"Hello, {greeting}!");
+    private string _greeting = "hello";
+
+    public override Task OnActivateAsync(CancellationToken cancellationToken)
+    {
+        Console.WriteLine("Activating Friend Grain " + DateTime.Now);
+        return base.OnActivateAsync(cancellationToken);
+    }
+
+    public ValueTask<string> SayHello(string greeting)
+    {
+        Console.WriteLine("Start " + DateTime.Now);
+        var d = Stopwatch.StartNew();
+        while (d.ElapsedMilliseconds < 5000)
+        {
+
+        }
+        _greeting = greeting;
+        Console.WriteLine("Finished Hello " + DateTime.Now);
+        return ValueTask.FromResult($"Hello, {greeting}!");
+    }
+
+    public ValueTask<string> GetGreeting()
+    {
+        Console.WriteLine("Retrieved Greetings "+ DateTime.Now);
+        return ValueTask.FromResult(_greeting);
+    }
 }
 
 public interface IHelloGrain : IGrainWithStringKey
 {
     ValueTask<string> SayHello(string greeting);
+    [AlwaysInterleave]
+    ValueTask<string> GetGreeting();
 }
--- Types/Program.cs
+++ Types/Program.cs
@@ -1,4 +1,7 @@
-﻿using Types;
+﻿using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.ObjectModel;
+using Types;
 
 //int number = 1;
 //int? nullable = null;
@@ -19,5 +22,65 @@
 //var result = number == number2; //false
 //var result2 = number.Equals(number2); //true
 
+//DateTime date = new DateTime(1111, 1, 1);
+//DateTimeOffset dateTimeOffset = new DateTimeOffset(date);
+//DateOnly dateOnly = new DateOnly(2020,1,1);
+//TimeOnly timeOnly = new TimeOnly(10, 10);
+
+//Enumerables!!
+
+//ArrayList arrayList = new ArrayList();
+//arrayList.Add(1);
+//arrayList.Add("hello world");
+
+//foreach (var item in arrayList)
+//{
+//    Console.WriteLine(item);
+//}
+
+
+int[] array = new int[2];
+var humans = new List<IHuman>();
+List<int> list = new List<int>(humans.Count);//set correct initial capacity
+
+for (int i = 0; i < humans.Count; i++)
+{
+    list.Add(i);
+}
+
+Collection<int> collection = new Collection<int>();
+Dictionary<int, string> dictionary = new Dictionary<int, string>(10);
+HashSet<int> hashSet = new HashSet<int>(10);
+Stack<int> stack = new Stack<int>(10);
+Queue<int> queue = new Queue<int>(10);
+ConcurrentDictionary<int, string> concurrentDictionary = new ConcurrentDictionary<int, string>();
+ConcurrentQueue<int> concurrentQueue = new ConcurrentQueue<int>();
+ConcurrentStack<int> concurrentStack = new ConcurrentStack<int>();
+
+//var student = new Student();
+//var teacher = new Teacher();
+
+//list.Add(student);
+//list.Add(teacher);
 
 Console.WriteLine();
+
+class Student : Human
+{
+
+}
+
+class Teacher : Human
+{
+
+}
+
+class Human : IHuman
+{
+
+}
+
+interface IHuman
+{
+
+}
