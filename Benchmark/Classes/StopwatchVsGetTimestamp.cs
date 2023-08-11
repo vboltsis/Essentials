615856d6a38ec63e95f99a17087d6b6400d46e44
Evangelos Boltsis <vbk1313@hotmail.com>
2023-08-11 19:43:33 +0300

    -added stopwatch benchmark

---
+++ Benchmark/Classes/StopwatchVsGetTimestamp.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+
+namespace Benchmark.Classes;
+
+/*
+|         Method |     Mean |   Error |  StdDev | Allocated |
+|--------------- |---------:|--------:|--------:|----------:|
+| StopwatchStart | 228.5 us | 0.41 us | 0.37 us |      40 B |
+|   GetTimestamp | 228.6 us | 0.63 us | 0.49 us |         - |
+*/
+
+[MemoryDiagnoser]
+public class StopwatchVsGetTimestamp
+{
+    private const int Million = 1_000_000;
+
+    [Benchmark]
+    public double StopwatchStart()
+    {
+        var stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        var sum = 0;
+        for (int i = 0; i < Million; i++)
+        {
+            sum += i;
+        }
+
+        stopwatch.Stop();
+
+        return stopwatch.Elapsed.TotalMilliseconds;
+    }
+
+    [Benchmark]
+    public double GetTimestamp()
+    {
+        var start = Stopwatch.GetTimestamp();
+
+        var sum = 0;
+        for (int i = 0; i < Million; i++)
+        {
+            sum += i;
+        }
+
+        var stop = Stopwatch.GetElapsedTime(start);
+
+        return stop.TotalMilliseconds;
+    }
+}
