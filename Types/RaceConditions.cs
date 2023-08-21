672f229b816b7662cd2da89de7acb9ae6afc4164
Evangelos Boltsis <v.boltsis@kaizengaming.com>
2023-08-21 12:04:56 +0300

    -added new example

---
+++ Types/RaceConditions.cs
@@ -0,0 +1,27 @@
+﻿namespace Types;
+
+public class RaceConditions
+{
+    public static void IncreaseNumber()
+    {
+        var number = 0;
+
+        for (int i = 0; i < 10_000; i++)
+        {
+            //fire and forget async
+            //tasks are not run in the same order as created
+            //we do not block any thread
+
+            Task.Run(() => 
+            {
+                Console.WriteLine("Hello from inside");
+                number++;
+            });
+
+            Console.WriteLine("Test");
+        }
+
+        Console.WriteLine(number);
+    }
+
+}
