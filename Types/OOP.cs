be875f29a34af3bba47bba928fbdf103b1cd65ef
Evangelos Boltsis <v.boltsis@kaizengaming.com>
2023-08-23 10:59:19 +0300

    -added oop class

---
+++ Types/OOP.cs
@@ -0,0 +1,67 @@
+﻿using System.Drawing;
+
+namespace Types;
+
+//access modifiers public, private, protected, internal, protected internal
+//abstract class, sealed class, static class
+
+//the default access modifier is internal for class and private for members
+
+//cannot be instantiated
+abstract class Animal : IAnimal
+{
+    public int Color { get; set; }
+    protected int Age { get; set; }
+    private int Id { get; set; }
+
+    public void Method()
+    {
+        Console.WriteLine("Method");
+    }
+
+    protected void Method2()
+    {
+        Console.WriteLine("Method2");
+    }
+
+    private void Method3()
+    {
+        Console.WriteLine("Method3");
+    }
+}
+
+//this is a contract to be implemented by the derived class
+internal interface IAnimal
+{
+    //default interface implementation
+    void Feed()
+    {
+        Console.WriteLine("Hello");
+    }
+}
+
+//cannot be inherited
+sealed class Dog : Animal
+{
+    public static void Bark()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+class Cat : Animal
+{
+    public static void Meow()
+    {
+        Console.WriteLine("Meow");
+    }
+}
+
+//cannot create an instance of a static class
+//cannot inherit from a static class
+static class StaticClass
+{
+    public static void Method()
+    {
+        Console.WriteLine("StaticClass");
+    }
+}
--- Types/Program.cs
+++ Types/Program.cs
@@ -4,6 +4,50 @@ using System.Collections.ObjectModel;
 using System.Diagnostics;
 using Types;
 
+//.NET FRAMEWORK 1 --> 2 --> 4.8.2 -- windows only
+//.NET CORE 1 --> 2.2 --> 3.1 --> 5.0 --> 6.0 --> 7.0 -- cross platform
+
+int number = 1;
+short number2 = 1;
+long number3 = 1;
+
+string text = "takis";
+char character = 't';
+
+bool boolean = true;
+bool boolean2 = false;
+bool boolean3 = true && false;//AND
+bool boolean4 = true || false;//OR
+bool boolean5 = !true;//NOT
+
+decimal decimalNumber = 1.1m;
+double doubleNumber = 1.1;
+float floatNumber = 1.1f;
+
+DateTime date = new DateTime(1111, 1, 1);
+DateTimeOffset dateTimeOffset = new DateTimeOffset(date);
+DateOnly dateOnly = new DateOnly(2020, 1, 1);
+TimeOnly timeOnly = new TimeOnly(1, 1, 1);
+
+int[] array = new int[10];
+List<int> list = new List<int> { 1, 2, 3 };
+Dictionary<int, string> dictionary = new Dictionary<int, string> { { 1, "1" }, { 2, "2" } };
+HashSet<int> hashSet = new HashSet<int> { 1, 2, 3, 1, 1, 1 };
+(int, string) tuple = (1, "1");
+
+//covariance
+List<IAnimal> animal = new List<IAnimal>
+{
+    new Dog(),
+    new Cat()
+};
+
+
+
+
+
+
+
 //int number = 1;
 //int? nullable = null;
 //DateTime date = new DateTime(1111, 1, 1);
@@ -31,21 +75,21 @@ using Types;
 //Enumerables!!
 
 //Plumber.WillThrowWhenRun();
-var res1 = Test.StopwatchStart();
-var res2 = Test.GetTimestamp();
+//var res1 = Test.StopwatchStart();
+//var res2 = Test.GetTimestamp();
 
-var array = new Takis[] { 
-    new Takis { Id = 3, Age = 3 },
-    new Takis { Id = 1, Age = 1 },
-    new Takis { Id = 2, Age = 2 },
-};
+//var array = new Takis[] { 
+//    new Takis { Id = 3, Age = 3 },
+//    new Takis { Id = 1, Age = 1 },
+//    new Takis { Id = 2, Age = 2 },
+//};
 
-Array.Sort(array, (a, b) => a.Id.CompareTo(b.Id));
+//Array.Sort(array, (a, b) => a.Id.CompareTo(b.Id));
 
-var numbers = new List<int> { 8, 2, 3, 4, 5, 6, 7, 1 };
-numbers.Sort();
+//var numbers = new List<int> { 8, 2, 3, 4, 5, 6, 7, 1 };
+//numbers.Sort();
 
-Console.ReadKey();
+//Console.ReadKey();
 
 struct Takis
 {
