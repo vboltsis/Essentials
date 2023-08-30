4cde9dcafc49c2f22ab49df67b9bd174e33a78d0
Evangelos Boltsis <v.boltsis@kaizengaming.com>
2023-08-30 10:41:20 +0300

    -added examples

---
+++ Types/ValueTaskExample.cs
@@ -0,0 +1,42 @@
+﻿namespace Types;
+
+public class ValueTaskExample
+{
+    public async Task<bool> GetSomethingAsync()
+    {
+        await Task.Delay(1000);
+
+        return true;
+    }
+
+    public async ValueTask<bool> GetValueTaskSomethingAsync()
+    {
+        var random = new Random();
+
+        if (random.Next(0, 2) == 0)
+        {
+            return false;
+        }
+
+        await Task.Delay(1000);
+
+        return true;
+    }
+
+    public async ValueTask<bool> Method1()
+    {
+        return await Method2();
+    }
+
+    public async ValueTask<bool> Method2()
+    {
+        return await Method3();
+    }
+
+    public async Task<bool> Method3()
+    {
+        await Task.Delay(1000);
+
+        return true;
+    }
+}
