c957338b658675259906c66216c6cd1103b720b3
Evangelos Boltsis <vbk1313@hotmail.com>
2023-10-15 23:51:13 +0300

    -Added moq tests

---
+++ XUnitTests/Moq/OrderServiceMoqTests.cs
@@ -0,0 +1,51 @@
+﻿using Moq;
+
+namespace XUnitTests.Moq;
+
+public class OrderServiceMoqTests
+{
+    private readonly Mock<IInventory> _mockInventory;
+    private readonly Mock<IPaymentGateway> _mockPaymentGateway;
+    private readonly OrderService _orderService;
+
+    public OrderServiceMoqTests()
+    {
+        _mockInventory = new Mock<IInventory>();
+        _mockPaymentGateway = new Mock<IPaymentGateway>();
+        _orderService = new OrderService(_mockInventory.Object, _mockPaymentGateway.Object);
+    }
+
+    [Fact]
+    public void ProcessOrder_WhenItemInStockAndPaymentSuccess_ReturnsTrue()
+    {
+        _mockInventory.Setup(inv => inv.IsInStock(It.IsAny<string>())).Returns(true);
+        _mockPaymentGateway.Setup(pg => pg.ProcessPayment(It.IsAny<double>())).Returns(true);
+
+        bool result = _orderService.ProcessOrder("item1", 100.0);
+
+        Assert.True(result);
+        _mockInventory.Verify(inv => inv.ReduceStock("item1"), Times.Once);
+    }
+
+    [Fact]
+    public void ProcessOrder_WhenItemInStockAndPaymentFails_ReturnsFalse()
+    {
+        _mockInventory.Setup(inv => inv.IsInStock(It.IsAny<string>())).Returns(true);
+        _mockPaymentGateway.Setup(pg => pg.ProcessPayment(It.IsAny<double>())).Returns(false);
+
+        bool result = _orderService.ProcessOrder("item1", 100.0);
+
+        Assert.False(result);
+        _mockInventory.Verify(inv => inv.ReduceStock("item1"), Times.Never);
+    }
+
+    [Fact]
+    public void ProcessOrder_WhenItemNotInStock_ReturnsFalse()
+    {
+        _mockInventory.Setup(inv => inv.IsInStock(It.IsAny<string>())).Returns(false);
+
+        bool result = _orderService.ProcessOrder("item1", 100.0);
+
+        Assert.False(result);
+    }
+}
