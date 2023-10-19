b50fc680aa2d0b12b35318b615a232969944d3c0
Evangelos Boltsis <vbk1313@hotmail.com>
2023-10-19 22:24:05 +0300

    -added benchmark

---
+++ Benchmark/Classes/PredicateVsFuncVsAction.cs
@@ -0,0 +1,30 @@
+﻿namespace Benchmark;
+
+/*
+|             Method |      Mean |     Error |    StdDev | Rank | Allocated |
+|------------------- |----------:|----------:|----------:|-----:|----------:|
+|      FuncBenchmark | 0.4660 ns | 0.0140 ns | 0.0109 ns |    2 |         - |
+| PredicateBenchmark | 0.4009 ns | 0.0224 ns | 0.0199 ns |    1 |         - |
+|    ActionBenchmark | 0.4696 ns | 0.0258 ns | 0.0229 ns |    2 |         - |
+*/
+
+[RankColumn]
+[MemoryDiagnoser]
+public class PredicateVsFuncVsAction
+{
+    private Func<int, int, int> divideFunc = (a, b) => a % b;
+    private Predicate<int> isEvenPredicate = number => number % 2 == 0;
+    private Action<int> doNothingAction = x => 
+    {
+        var y = x % 2;
+    };
+
+    [Benchmark]
+    public int FuncBenchmark() => divideFunc(4, 2);
+
+    [Benchmark]
+    public bool PredicateBenchmark() => isEvenPredicate(4);
+
+    [Benchmark]
+    public void ActionBenchmark() => doNothingAction(4);
+}
