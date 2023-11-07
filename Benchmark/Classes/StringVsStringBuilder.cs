508796b5c8450dce296c080613e75a2a16a10901
Evangelos Boltsis <v.boltsis@kaizengaming.com>
2023-11-07 18:15:52 +0200

    -updated benchmark

--- Benchmark/Classes/StringVsStringBuilder.cs
+++ Benchmark/Classes/StringVsStringBuilder.cs
@@ -1,18 +1,22 @@
-﻿using System.Text;
+﻿using Microsoft.Extensions.ObjectPool;
+using System.Text;
 
 namespace Benchmark.Classes;
 /*
-|             Method |   N |         Mean |      Error |     StdDev | Ratio |   Gen0 |   Gen1 | Allocated | Alloc Ratio |
-|------------------- |---- |-------------:|-----------:|-----------:|------:|-------:|-------:|----------:|------------:|
-| LogFileOfferString |   1 |    354.28 ns |   2.496 ns |   2.213 ns |  1.00 | 0.0720 |      - |     904 B |        1.00 |
-|       LogFileOffer |   1 |     46.39 ns |   0.489 ns |   0.544 ns |  0.13 | 0.0223 |      - |     280 B |        0.31 |
-|                    |     |              |            |            |       |        |        |           |             |
-| LogFileOfferString |  10 |  2,392.61 ns |  21.128 ns |  17.643 ns |  1.00 | 0.4005 |      - |    5064 B |        1.00 |
-|       LogFileOffer |  10 |    251.85 ns |   2.698 ns |   2.391 ns |  0.11 | 0.0591 |      - |     744 B |        0.15 |
-|                    |     |              |            |            |       |        |        |           |             |
-| LogFileOfferString | 100 | 23,330.02 ns | 305.532 ns | 428.314 ns |  1.00 | 3.8757 | 0.0916 |   48801 B |        1.00 |
-|       LogFileOffer | 100 |  2,028.05 ns |  18.789 ns |  16.656 ns |  0.09 | 0.3242 | 0.0038 |    4104 B |        0.08 |
- */
+| Method                        | _number | Mean         | Error      | StdDev     | Ratio | Gen0   | Allocated | Alloc Ratio |
+|------------------------------ |-------- |-------------:|-----------:|-----------:|------:|-------:|----------:|------------:|
+| LogFileOfferString            | 1       |    401.49 ns |   7.006 ns |   6.211 ns |  1.00 | 0.0720 |     904 B |        1.00 |
+| LogFileOffer                  | 1       |     51.39 ns |   0.934 ns |   0.959 ns |  0.13 | 0.0223 |     280 B |        0.31 |
+| LogFileOfferStringBuilderPool | 1       |     36.28 ns |   0.296 ns |   0.277 ns |  0.09 | 0.0057 |      72 B |        0.08 |
+|                               |         |              |            |            |       |        |           |             |
+| LogFileOfferString            | 10      |  2,621.33 ns |  24.822 ns |  22.004 ns |  1.00 | 0.4005 |    5064 B |        1.00 |
+| LogFileOffer                  | 10      |    288.06 ns |   5.811 ns |   7.136 ns |  0.11 | 0.0591 |     744 B |        0.15 |
+| LogFileOfferStringBuilderPool | 10      |    232.39 ns |   1.111 ns |   1.040 ns |  0.09 | 0.0157 |     200 B |        0.04 |
+|                               |         |              |            |            |       |        |           |             |
+| LogFileOfferString            | 100     | 25,509.03 ns | 285.794 ns | 267.332 ns |  1.00 | 3.8757 |   48801 B |        1.00 |
+| LogFileOffer                  | 100     |  2,195.57 ns |  17.243 ns |  16.935 ns |  0.09 | 0.3242 |    4104 B |        0.08 |
+| LogFileOfferStringBuilderPool | 100     |  1,998.10 ns |  19.598 ns |  16.365 ns |  0.08 | 0.1221 |    1552 B |        0.03 |
+*/
 
 [MemoryDiagnoser]
 public class StringVsStringBuilder
@@ -20,6 +24,13 @@ public class StringVsStringBuilder
     [Params(1, 10, 100)]
     public int _number;
     private List<FileManager> _files { get; set; }
+
+    private static readonly ObjectPool<StringBuilder> _stringBuilderPool =
+        new DefaultObjectPool<StringBuilder>(new StringBuilderPooledObjectPolicy
+        {
+            InitialCapacity = 256,
+        }, 100);
+
 
     [GlobalSetup]
     public void GlobalSetup()
@@ -64,6 +75,33 @@ public class StringVsStringBuilder
         }
 
         return builder.ToString();
+    }
+
+    [Benchmark]
+    public string LogFileOfferStringBuilderPool()
+    {
+        var separator = ": ";
+        var builder = _stringBuilderPool.Get();
+        builder.AppendLine("File Offers");
+        foreach (var file in _files)
+        {
+            builder.Append(file.Id);
+            builder.Append(separator);
+            if (file.FileError?.FileErrorMetadata?.FileAmount.HasValue == true)
+            {
+                builder.Append(file.FileError.FileErrorMetadata.FileAmount.Value);
+            }
+            else
+            {
+                builder.Append(file.FileError.FileErrorMetadata.Error);
+            }
+            builder.Append(Environment.NewLine);
+        }
+
+        var result = builder.ToString();
+        builder.Clear();
+        _stringBuilderPool.Return(builder);
+        return result;
     }
 
     private static List<FileManager> GetFileManagers(int amount)
