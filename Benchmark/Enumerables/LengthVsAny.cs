8ba21ecc98d3114116fb17e9300055b54a1d92bb
Evangelos Boltsis <v.boltsis@kaizengaming.com>
2024-01-02 18:06:04 +0200

    -updated benchmark

--- Benchmark/Enumerables/LengthVsAny.cs
+++ Benchmark/Enumerables/LengthVsAny.cs
@@ -3,17 +3,15 @@
 /*
 | Method | Mean      | Error     | StdDev    | Median    | Ratio | RatioSD | Rank | Allocated | Alloc Ratio |
 |------- |----------:|----------:|----------:|----------:|------:|--------:|-----:|----------:|------------:|
-| Length | 0.0058 ns | 0.0099 ns | 0.0083 ns | 0.0000 ns |     ? |       ? |    1 |         - |           ? |
-| Any    | 6.3964 ns | 0.1525 ns | 0.1498 ns | 6.3528 ns |     ? |       ? |    2 |         - |           ? |
+| Length | 0.0658 ns | 0.0257 ns | 0.0275 ns | 0.0494 ns |  1.00 |    0.00 |    1 |         - |          NA |
+| Any    | 5.8000 ns | 0.0108 ns | 0.0101 ns | 5.8010 ns | 97.47 |   37.33 |    2 |         - |          NA |
  */
 
 [RankColumn]
 [MemoryDiagnoser]
 public class LengthVsAny
 {
-    private const int N = 1000;
-
-    private readonly int[] _array = new int[N];
+    private readonly int[] _array = [1, 2, 3, 4];
 
     [Benchmark(Baseline = true)]
     public bool Length()
