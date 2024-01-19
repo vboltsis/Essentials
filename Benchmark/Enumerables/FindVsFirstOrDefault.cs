ddae557deea4a7132eb7faae66b214e79d76f938
Evangelos Boltsis <v.boltsis@kaizengaming.com>
2024-01-19 17:44:54 +0200

    .NET 8 results

--- Benchmark/Enumerables/FindVsFirstOrDefault.cs
+++ Benchmark/Enumerables/FindVsFirstOrDefault.cs
@@ -1,12 +1,12 @@
 ﻿namespace Benchmark;
 
 /*
-|             Method | Number |      Mean |     Error |    StdDev | Rank |   Gen0 | Allocated |
+| Method             | Number | Mean      | Error     | StdDev    | Rank | Gen0   | Allocated |
 |------------------- |------- |----------:|----------:|----------:|-----:|-------:|----------:|
-|           FindList |     10 |  9.250 ns | 0.0194 ns | 0.0172 ns |    1 |      - |         - |
-| FirstOrDefaultList |     10 | 52.044 ns | 0.9413 ns | 0.7860 ns |    3 | 0.0048 |      40 B |
-|           FindCars |     10 | 13.931 ns | 0.0463 ns | 0.0433 ns |    2 |      - |         - |
-| FirstOrDefaultCars |     10 | 71.897 ns | 1.1182 ns | 0.9338 ns |    4 | 0.0048 |      40 B |
+| FindList           | 10     |  3.960 ns | 0.0434 ns | 0.0362 ns |    1 |      - |         - |
+| FirstOrDefaultList | 10     | 15.197 ns | 0.1253 ns | 0.1046 ns |    3 | 0.0032 |      40 B |
+| FindCars           | 10     |  6.676 ns | 0.1416 ns | 0.2366 ns |    2 |      - |         - |
+| FirstOrDefaultCars | 10     | 46.180 ns | 0.3461 ns | 0.2890 ns |    4 | 0.0032 |      40 B |
  */
 
 [MemoryDiagnoser]
