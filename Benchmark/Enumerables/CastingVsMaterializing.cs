baf1ad50a339f61c4e9d4d56fb747216a301789b
Evangelos Boltsis <v.boltsis@kaizengaming.com>
2024-01-26 14:50:06 +0200

    -bug?

---
+++ Benchmark/Enumerables/CastingVsMaterializing.cs
@@ -0,0 +1,35 @@
+﻿namespace Benchmark;
+
+[MemoryDiagnoser]
+public class CastingVsMaterializing
+{
+    private static IEnumerable<int> _list = new int[] { 1, 2, 3, 4, 5 };
+
+    [Benchmark(Baseline = true)]
+    public int Casting()
+    {
+        var array = _list as int[];
+        var sum = 0;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            sum += array[i];
+        }
+
+        return sum;
+    }
+
+    [Benchmark]
+    public int Materializing()
+    {
+        var array = _list.ToArray();
+        var sum = 0;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            sum += array[i];
+        }
+
+        return sum;
+    }
+}
--- Types/Program.cs
+++ Types/Program.cs
@@ -1,5 +1,32 @@
 ﻿using FeatureExamples;
 
-UpperCaseStringCollection.Example();
+//UpperCaseStringCollection.Example();
 Console.WriteLine();
 
+var myClass = new FirstClass
+{
+    SecondClass = new SecondClass
+    {
+        Id = 0
+    }
+};
+
+
+if (myClass.SecondClass?.Id != default)
+{
+    Console.WriteLine("it is not 0");
+}
+else
+{
+    Console.WriteLine("it is 0");
+}
+
+class FirstClass
+{
+    public SecondClass SecondClass { get; set; }
+}
+
+class SecondClass
+{
+    public int Id { get; set; }
+}
