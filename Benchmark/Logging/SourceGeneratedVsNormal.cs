2ee0848a04ca81c21286b816424b948cb2bedd1b
Evangelos Boltsis <vbk1313@hotmail.com>
2024-09-07 11:14:31 +0300

    -added source generated logs

---
+++ Benchmark/Logging/SourceGeneratedVsNormal.cs
@@ -0,0 +1,211 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Configuration;
+using Microsoft.Extensions.Logging.Console;
+
+namespace Benchmark;
+
+/*
+| Method                                       | Mean     | Error    | StdDev   | Gen0   | Allocated |
+|--------------------------------------------- |---------:|---------:|---------:|-------:|----------:|
+| Log_WithoutIf_WithParams                     | 55.55 ns | 0.316 ns | 0.280 ns | 0.0105 |      88 B |
+| Log_WithIf_WithParams                        | 56.74 ns | 0.437 ns | 0.409 ns | 0.0105 |      88 B |
+| LogAdapter_WithoutIf_WithParams              | 61.47 ns | 0.448 ns | 0.397 ns | 0.0105 |      88 B |
+| LoggerMessageDef_WithoutIf_WithParams        | 28.52 ns | 0.223 ns | 0.208 ns |      - |         - |
+| LoggerMessage_SourceGen_WithoutIf_WithParams | 23.74 ns | 0.099 ns | 0.088 ns |      - |         - | 
+*/
+
+[MemoryDiagnoser]
+public class SourceGeneratedVsNormal
+{
+    private const string LogMessageWithParameters =
+        "This is a log message with parameters {First}, {Second}";
+
+    private readonly ILoggerFactory _loggerFactory = LoggerFactory.Create(builder =>
+    {
+        builder.AddFakeLogger().SetMinimumLevel(LogLevel.Information);
+    });
+
+    private readonly ILogger<SourceGeneratedVsNormal> _logger;
+    private readonly ILoggerAdapter<SourceGeneratedVsNormal> _loggerAdapter;
+
+    public SourceGeneratedVsNormal()
+    {
+        _logger = new Logger<SourceGeneratedVsNormal>(_loggerFactory);
+        _loggerAdapter = new LoggerAdapter<SourceGeneratedVsNormal>(_logger);
+    }
+
+    [Benchmark]
+    public void Log_WithoutIf_WithParams()
+    {
+        _logger.LogInformation(LogMessageWithParameters, 69, 420);
+    }
+
+    [Benchmark]
+    public void Log_WithIf_WithParams()
+    {
+        if (_logger.IsEnabled(LogLevel.Information))
+        {
+            _logger.LogInformation(LogMessageWithParameters, 69, 420);
+        }
+    }
+
+    [Benchmark]
+    public void LogAdapter_WithoutIf_WithParams()
+    {
+        _loggerAdapter.LogInformation(LogMessageWithParameters, 69, 420);
+    }
+
+    [Benchmark]
+    public void LoggerMessageDef_WithoutIf_WithParams()
+    {
+        _logger.LogBenchmarkMessage(69, 420);
+    }
+
+    [Benchmark]
+    public void LoggerMessage_SourceGen_WithoutIf_WithParams()
+    {
+        _logger.LogBenchmarkMessageGen(69, 420);
+    }
+
+}
+
+public static class LoggerMessageDefinitions
+{
+    private static readonly Action<ILogger, int, int, Exception?> BenchmarkedLogMessageDefinition =
+        LoggerMessage.Define<int, int>(LogLevel.Information, 0,
+            "This is a log message with parameters {First}, {Second}");
+
+    public static void LogBenchmarkMessage(this ILogger logger, int first, int second)
+    {
+        BenchmarkedLogMessageDefinition(logger, first, second, null);
+    }
+}
+
+public static partial class LoggerMessageDefinitionsGen
+{
+    [LoggerMessage(EventId = EventIdConstants.BenchmarkLogMessage, Level = LogLevel.Information,
+        Message = "This is a log message with parameters {First}, {Second}",
+        SkipEnabledCheck = true)]
+    public static partial void LogBenchmarkMessageGen(this ILogger logger, int first, int second);
+}
+
+public class LoggerAdapter<T> : ILoggerAdapter<T>
+{
+    private readonly ILogger<T> _logger;
+
+    public LoggerAdapter(ILogger<T> logger)
+    {
+        _logger = logger;
+    }
+
+    public void LogInformation(string message)
+    {
+        if (_logger.IsEnabled(LogLevel.Information))
+        {
+            _logger.LogInformation(message);
+        }
+    }
+
+    public void LogInformation<T0>(string message, T0 arg0)
+    {
+        if (_logger.IsEnabled(LogLevel.Information))
+        {
+            _logger.LogInformation(message, arg0);
+        }
+    }
+
+    public void LogInformation<T0, T1>(string message, T0 arg0, T1 arg1)
+    {
+        if (_logger.IsEnabled(LogLevel.Information))
+        {
+            _logger.LogInformation(message, arg0, arg1);
+        }
+    }
+
+    public void LogInformation<T0, T1, T2>(string message, T0 arg0, T1 arg1, T2 arg2)
+    {
+        if (_logger.IsEnabled(LogLevel.Information))
+        {
+            _logger.LogInformation(message, arg0, arg1, arg2);
+        }
+    }
+}
+
+public interface ILoggerAdapter<T>
+{
+    void LogInformation(string message);
+
+    void LogInformation<T0>(string message, T0 arg0);
+
+    void LogInformation<T0, T1>(string message, T0 arg0, T1 arg1);
+
+    void LogInformation<T0, T1, T2>(string message, T0 arg0, T1 arg1, T2 arg2);
+}
+
+public class FakeLoggingProvider : ILoggerProvider
+{
+    public void Dispose()
+    {
+
+    }
+
+    public ILogger CreateLogger(string categoryName)
+    {
+        return new FakeLogger();
+    }
+}
+
+public class FakeLogger : ILogger
+{
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+    {
+
+    }
+
+    public bool IsEnabled(LogLevel logLevel)
+    {
+        return logLevel != LogLevel.None;
+    }
+
+    public IDisposable BeginScope<TState>(TState state)
+    {
+        return new FakeDisposable();
+    }
+
+    private class FakeDisposable : IDisposable
+    {
+        public void Dispose()
+        {
+        }
+    }
+}
+
+public static class FakeLoggerExtensions
+{
+    public static ILoggingBuilder AddFakeLogger(this ILoggingBuilder builder)
+    {
+        builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<ILoggerProvider, FakeLoggingProvider>());
+        LoggerProviderOptions.RegisterProviderOptions<ConsoleLoggerOptions, FakeLoggingProvider>(builder.Services);
+        return builder;
+    }
+}
+
+public static class EventIdConstants
+{
+    public const int BenchmarkLogMessage = 0;
+}
+
+/* HOW TO TEST IN PROGRAM.CS
+using var loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder.AddConsole(); // Adds console logging
+    builder.SetMinimumLevel(LogLevel.Information); // Set minimum logging level
+});
+
+// Create a logger instance
+ILogger logger = loggerFactory.CreateLogger<Program>();
+
+logger.LogBenchmarkMessageGen(1, 2);
+*/
--- PollyExample/Program.cs
+++ PollyExample/Program.cs
@@ -1,3 +1,6 @@
+using Polly;
+using Polly.Retry;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
@@ -9,18 +12,82 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
+//old way
+app.MapGet("/weatherforecastold", async () =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    var policy = new ClientRetryPolicy();
+
+    var client = new HttpClient();
+
+    var response = await policy.JustHttpRetry.ExecuteAsync(() =>
+    {
+        return client.GetAsync("https://localhost:7133/notify/sms/test");
+    });
 })
-.WithName("GetWeatherForecast");
+.WithName("GetWeatherForecastOld");
+
+//new way
+app.MapGet("/weatherforecast", async (CancellationToken token) =>
+{
+    ResiliencePipeline resiliencePipeline = new ResiliencePipelineBuilder()
+        .AddRetry(new RetryStrategyOptions
+        {
+            OnRetry = static args =>
+            {
+                Console.WriteLine("OnRetry, Attempt: {0}", args.AttemptNumber);
+
+                // Event handlers can be asynchronous; here, we return an empty ValueTask.
+                return default;
+            },
+            ShouldHandle = new PredicateBuilder().Handle<Exception>(),
+            Delay = TimeSpan.FromSeconds(1),
+            MaxRetryAttempts = 3,
+            BackoffType = DelayBackoffType.Linear
+            //BackoffType = DelayBackoffType.Constant
+            //BackoffType = DelayBackoffType.Exponential
+        })
+        .Build();
+
+    var result = await resiliencePipeline.ExecuteAsync(static async token =>
+    {
+        var client = new HttpClient();
+        var response = await client.GetAsync("https://localhost:7133/notify/sms/test", token);
+
+        return response;
+    }, token);
+
+    return result;
+});
+
+
 
 app.Run();
+
+
+
+
+/// <summary>
+/// Old way with Polly
+/// </summary>
+public class ClientRetryPolicy
+{
+    public AsyncRetryPolicy<HttpResponseMessage> JustHttpRetry { get; set; }
+    public AsyncRetryPolicy<HttpResponseMessage> HttpRetryWithWaiting { get; set; }
+    public AsyncRetryPolicy<HttpResponseMessage> ExponentialHttpRetry { get; set; }
+
+    public ClientRetryPolicy()
+    {
+        JustHttpRetry = Policy.HandleResult<HttpResponseMessage>(
+            response => !response.IsSuccessStatusCode)
+            .RetryAsync(3);
+
+        HttpRetryWithWaiting = Policy.HandleResult<HttpResponseMessage>(
+            response => !response.IsSuccessStatusCode)
+            .WaitAndRetryAsync(3, attempt => TimeSpan.FromSeconds(5));
+
+        ExponentialHttpRetry = Policy.HandleResult<HttpResponseMessage>(
+            response => !response.IsSuccessStatusCode)
+            .WaitAndRetryAsync(3,
+                attempt => TimeSpan.FromSeconds(Math.Pow(2, attempt)));
+    }
+}
