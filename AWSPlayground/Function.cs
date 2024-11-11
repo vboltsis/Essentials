682f930fe0809c5565050558ab7705c29fe1be04
Evangelos Boltsis <vbk1313@hotmail.com>
2024-11-11 09:26:05 +0200

    -Added AWS Lambda example -Added Goto example

---
+++ AWSPlayground/Function.cs
@@ -0,0 +1,65 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace AWSPlayground;
+
+public class Function
+{
+    private static readonly AmazonDynamoDBClient _client =
+        new AmazonDynamoDBClient("AccessKey", "SecretKey",
+            new AmazonDynamoDBConfig
+        {
+            RegionEndpoint = Amazon.RegionEndpoint.EUCentral1
+        });
+
+    private static readonly Table _table = new TableBuilder(_client, "Products")
+        .AddHashKey("ProductId", DynamoDBEntryType.String)
+        .Build();
+
+    public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest request)
+    {
+        Console.WriteLine("FunctionHandler called");
+
+        var product = JsonSerializer.Deserialize<Product>(request.Body);
+
+        if (product == null)
+        {
+            return new APIGatewayProxyResponse
+            {
+                StatusCode = 400,
+                Body = "Invalid product data."
+            };
+        }
+
+        var document = new Document
+        {
+            ["ProductId"] = product.ProductId,
+            ["Name"] = product.Name,
+            ["Price"] = product.Price
+        };
+
+        await _table.PutItemAsync(document);
+
+        Console.WriteLine("Product added successfully!");
+
+        return new APIGatewayProxyResponse
+        {
+            StatusCode = 200,
+            Body = "Product added successfully!"
+        };
+    }
+}
+
+public class Product
+{
+    public string ProductId { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+}
---
+++ Types/Examples/GoToExample.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeatureExamples;
+
+public class GoToExample
+{
+    public void ProcessData()
+    {
+        var a = 1;
+        if (a == 1)
+            goto Error;
+
+        PerformSomeAction();
+        return;
+
+    Error:
+        LogError();
+    }
+
+    private void LogError()
+    {
+        Console.WriteLine("Error");
+    }
+
+    private void PerformSomeAction()
+    {
+        Console.WriteLine("hello");
+    }
+}
