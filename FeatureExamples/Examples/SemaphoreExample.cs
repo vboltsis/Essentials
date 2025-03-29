d6dacfc770f3db7cf79d47a07b2ad4adf185d54c
Evangelos Boltsis <vbk1313@hotmail.com>
2025-03-29 21:58:14 +0200

    added semaphore example

---
+++ FeatureExamples/Examples/SemaphoreExample.cs
@@ -0,0 +1,37 @@
+﻿namespace FeatureExamples;
+
+public class SemaphoreExample
+{
+    // Create a SemaphoreSlim that allows up to 3 concurrent threads.
+    private static readonly SemaphoreSlim semaphore = new SemaphoreSlim(3);
+
+    public static async Task Example()
+    {
+        Task[] tasks = new Task[10];
+        for (int i = 0; i < tasks.Length; i++)
+        {
+            int taskId = i; // Local copy for closure safety
+            tasks[i] = Task.Run(() => DoWorkAsync(taskId));
+        }
+
+        await Task.WhenAll(tasks);
+        Console.WriteLine("All tasks have completed.");
+    }
+
+    static async Task DoWorkAsync(int id)
+    {
+        Console.WriteLine($"Task {id} is waiting to enter...");
+        await semaphore.WaitAsync();
+        try
+        {
+            Console.WriteLine($"Task {id} entered the semaphore.");
+            // Simulate some work.
+            await Task.Delay(10000);
+        }
+        finally
+        {
+            semaphore.Release();
+            Console.WriteLine($"Task {id} released the semaphore.");
+        }
+    }
+}
--- FeatureExamples/Program.cs
+++ FeatureExamples/Program.cs
@@ -1,25 +1,3 @@
-﻿using System;
-using System.Runtime.CompilerServices;
+﻿using FeatureExamples;
 
-public class Logger
-{
-    public void Log(string message, [CallerMemberName] string callerName = "")
-    {
-        Console.WriteLine($"[{callerName}] {message}");
-    }
-}
-
-class Program
-{
-    static void Main()
-    {
-        var logger = new Logger();
-        logger.Log("Hello, world!");
-        MyMethod(logger);
-    }
-
-    static void MyMethod(Logger logger)
-    {
-        logger.Log("Inside MyMethod");
-    }
-}
+await SemaphoreExample.Example();
